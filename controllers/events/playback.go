d5e86244d110837982372fa8c9987ec4fcc9f602
Bryan Boreham <bjboreham@gmail.com>
2021-05-06 11:15:49 +0000

    Update playback.go

--- controllers/events/playback.go
+++ controllers/events/playback.go
@@ -1,112 +1,156 @@
 package events
 
 import (
+	"bytes"
 	"context"
+	gojson "encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"os"
 	"time"
 
-	"github.com/weaveworks/libgitops/pkg/serializer"
+	"github.com/weaveworks-experiments/kspan/pkg/mtime"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	k8sserializer "k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
+type captureDetails struct {
+	Timestamp time.Time `json:"time"`
+	Style     string    `json:"style"`
+	Kind      string    `json:"kind"`
+}
+
 // Capture details on an object, so we can play back sequences for testing
-func (r *EventWatcher) captureObject(obj runtime.Object) {
+func (r *EventWatcher) captureObject(obj runtime.Object, style string) {
 	if r.Capture == nil {
 		return
 	}
 	fmt.Fprintln(r.Capture, "---")
-	fmt.Fprintln(r.Capture, "#", time.Now().Format(time.RFC3339))
-	s := serializer.NewSerializer(r.scheme, nil)
-	fw := serializer.NewYAMLFrameWriter(r.Capture)
-	_ = s.Encoder().Encode(fw, obj)
+	d := captureDetails{Timestamp: mtime.Now(), Style: style, Kind: obj.GetObjectKind().GroupVersionKind().Kind}
+	buf, _ := gojson.Marshal(&d)
+	fmt.Fprintln(r.Capture, "#", string(buf))
+	cf := k8sserializer.NewCodecFactory(r.scheme)
+	serializerInfo, _ := runtime.SerializerInfoForMediaType(cf.SupportedMediaTypes(), runtime.ContentTypeYAML)
+	encoder := serializerInfo.Serializer
+	err := encoder.Encode(obj, r.Capture)
+	_ = err // TODO error handling
 }
 
-func getInitialObjects(filename string) ([]runtime.Object, error) {
-	scheme := runtime.NewScheme()
-	_ = clientgoscheme.AddToScheme(scheme)
-	s := serializer.NewSerializer(scheme, nil)
+func walkFile(ctx context.Context, filename string, callback func(captureDetails, []byte) error) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("error opening %q: %v", filename, err)
 	}
 	defer file.Close()
-	fr := serializer.NewYAMLFrameReader(file)
-	objects := make(map[types.UID]runtime.Object)
+	fr := json.YAMLFramer.NewFrameReader(file)
+	buf := make([]byte, 64*1024) // "should be enough for anyone"
 	for {
-		obj, err := s.Decoder().Decode(fr)
-		if err != nil {
-			if !errors.Is(err, io.EOF) {
-				return nil, err
-			}
-			break
-		}
-
-		ma, _ := meta.Accessor(obj)
-		gvk := obj.GetObjectKind().GroupVersionKind()
-		if gvk.Kind == "Event" {
-			continue
-		}
-		if _, exists := objects[ma.GetUID()]; exists {
-			continue
-		}
-		fmt.Println(gvk.Kind, ma.GetNamespace(), ma.GetName())
-		objects[ma.GetUID()] = obj
-	}
-
-	ret := make([]runtime.Object, 0, len(objects))
-	for _, v := range objects {
-		ret = append(ret, v)
-	}
-	return ret, nil
-}
-
-func playback(ctx context.Context, r *EventWatcher, filename string) {
-	s := serializer.NewSerializer(r.scheme, nil)
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("error opening %q: %v", filename, err)
-		return
-	}
-	defer file.Close()
-	fr := serializer.NewYAMLFrameReader(file)
-	for {
-		obj, err := s.Decoder().Decode(fr)
+		n, err := fr.Read(buf)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				break
 			}
-			fmt.Printf("error: %v", err)
-			return
+			return fmt.Errorf("error: %v", err)
 		}
 
-		gvk := obj.GetObjectKind().GroupVersionKind()
-		if gvk.Kind == "Event" {
-			err = r.handleEvent(ctx, obj.(*v1.Event))
-		} else {
-			err = r.watcher.playback(ctx, obj, r)
+		doc := buf[:n]
+		if n < 10 {
+			return fmt.Errorf("doc too short: %q", string(doc))
 		}
+		if bytes.HasPrefix(doc, []byte("---\n")) {
+			doc = doc[4:]
+		}
+
+		// we expect the top line of each doc to have metadata written out by capture
+		line := firstLine(doc)
+		var details captureDetails
+		err = gojson.Unmarshal(line[2:], &details)
 		if err != nil {
-			fmt.Printf("error: %v", err)
+			return fmt.Errorf("error parsing first line %q: %v", string(line), err)
+		}
+
+		err = callback(details, doc)
+		if err != nil {
+			return err
 		}
 	}
+	return nil
 }
 
-func (m *watchManager) playback(ctx context.Context, obj runtime.Object, ew eventNotifier) error {
-	ma, _ := meta.Accessor(obj)
-	ref := refFromObject(ma)
-	m.Lock()
-	wi := m.watches[ref]
-	m.Unlock()
-	if wi == nil {
-		return fmt.Errorf("watcher not found for object %q", ref)
+func firstLine(buf []byte) []byte {
+	p := bytes.IndexByte(buf, '\n')
+	if p == -1 {
+		return nil
 	}
-	return wi.checkConditionUpdates(obj.(*unstructured.Unstructured), ew)
+	return buf[:p]
+}
+
+func getInitialObjects(filename string) ([]runtime.Object, time.Time, error) {
+	scheme := runtime.NewScheme()
+	_ = clientgoscheme.AddToScheme(scheme)
+	//s := serializer.NewSerializer(scheme, nil)
+	var maxTimestamp time.Time
+	var objects []runtime.Object
+
+	err := walkFile(context.Background(), filename, func(details captureDetails, doc []byte) error {
+		if details.Timestamp.After(maxTimestamp) {
+			maxTimestamp = details.Timestamp
+		}
+		switch details.Style {
+		case "initial":
+			dec := yaml.NewYAMLToJSONDecoder(bytes.NewBuffer(doc))
+			var u unstructured.Unstructured
+			err := dec.Decode(&u)
+			if err != nil {
+				return err
+			}
+			objects = append(objects, &u)
+		}
+		return nil
+	})
+	return objects, maxTimestamp, err
+}
+
+func playback(ctx context.Context, r *EventWatcher, filename string) error {
+	return walkFile(ctx, filename, func(details captureDetails, doc []byte) error {
+		switch details.Style {
+		case "initial":
+			// no-op on playback
+		case "event":
+			dec := yaml.NewYAMLToJSONDecoder(bytes.NewBuffer(doc))
+			var ev v1.Event
+			err := dec.Decode(&ev)
+			if err != nil {
+				return fmt.Errorf("error parsing: %v", err)
+			}
+			mtime.NowForce(details.Timestamp)
+			err = r.handleEvent(ctx, &ev)
+			mtime.NowReset()
+			return err
+		case "watch":
+			dec := yaml.NewYAMLToJSONDecoder(bytes.NewBuffer(doc))
+			var u unstructured.Unstructured
+			err := dec.Decode(&u)
+			if err != nil {
+				return fmt.Errorf("error parsing: %v", err)
+			}
+			mtime.NowForce(details.Timestamp)
+			wi, err := r.watcher.getWatch(&u)
+			if err != nil {
+				return err
+			}
+			err = wi.checkConditionUpdates(&u, r)
+			mtime.NowReset()
+			return err
+		default:
+			return fmt.Errorf("style not recognized: %q", string(details.Style))
+		}
+		return nil
+	})
 }
